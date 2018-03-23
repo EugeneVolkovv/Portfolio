4d3b217da739e4e4c4fb872a9604816c7a53c137
EugeneVolkovv <eugene.volkoff78@gmail.com>
2018-03-23 23:45:27 +0300

    Add files via upload

---
+++ Unity3d(multiple shooter)/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+[System.Serializable]
+public class NewBehaviourScript  {
+
+	public string name = "Пистолет";	
+	public float damage = 10f;
+	public float range = 100f;
+}
---
+++ Unity3d(multiple shooter)/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    [SerializeField]
+    private float maxHealth = 100f;
+
+    [SyncVar]
+    private float currHealth;
+
+    void Awake(){
+        currHealth = maxHealth;
+    }
+
+    public void TakeDamage (float damage){
+        currHealth -= damage;
+
+        Debug.Log(transform.name + "имеет уровень здоровья" + currHealth);
+    }
+}
